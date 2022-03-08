85f878040b0fbe14499c1cf4aa2684a9c60a8b1e
Scott Robison <scott.robison@AFSWWCCR25R7UN.us.accenturefederal.com>
2022-03-08 13:27:03 -0600

    Renamed variables to follow naming conventions

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Game.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Game.java
@@ -5,36 +5,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class Game {
 
-	/**
-	 * TODO 1.0 java object member variable naming convention declared here are not object oriented,
-	 *  rename the variables, and their setter/getter methods to match convention. 
-	 */
-	private Long game_id;
-	private String game_name;
-	private String game_genre;
+	private Long id;
+	private String name;
+	private String genre;
 
 	public Long getGame_id() {
-		return game_id;
+		return id;
 	}
 
 	public void setGame_id(Long game_id) {
-		this.game_id = game_id;
+		this.id = game_id;
 	}
 
 	public String getGame_name() {
-		return game_name;
+		return name;
 	}
 
 	public void setGame_name(String game_name) {
-		this.game_name = game_name;
+		this.name = game_name;
 	}
 
 	public String getGame_genre() {
-		return game_genre;
+		return genre;
 	}
 
 	public void setGame_genre(String game_genre) {
-		this.game_genre = game_genre;
+		this.genre = game_genre;
 	}
 
 }
