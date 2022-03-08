1e31ab3b0061cbb6dfe030ee4ac1535ca096ecc3
Scott Robison <scott.robison@AFSWWCCR25R7UN.us.accenturefederal.com>
2022-03-08 13:18:05 -0600

    Renamed interface "GameService" to follow naming conventions

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/service/GameServiceImpl.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/service/GameServiceImpl.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.organization.mvcproject.MGL_Task1.model.Game;
 
-//TODO 1.0  follow java class naming, improve class name
 @Service
-public class GameServiceImpl implements Game_Service {
+public class GameServiceImpl implements GameService {
 
 	/**
 	 * TODO 2.0 The class that interacts with persistent data is called a Data Access Object(DAO)
